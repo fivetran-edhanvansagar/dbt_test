21e07f736a3787314c63b85b6131092568c1b98a
fivetran-edhanvansagar <160458963+fivetran-edhanvansagar@users.noreply.github.com>
2024-12-02 00:39:44 +0530

    Delete target/run/dbttest/models/example/mytransf.sql

--- target/run/dbttest/models/example/mytransf.sql
+++
@@ -1,7 +0,0 @@
-
-  create or replace   view DHANVAN_TEST.GOOGLE_CLOUD_MYSQL_TEST.mytransf
-  
-   as (
-    select * from DHANVAN_TEST.google_cloud_mysql_test.JUST_A_TABLE where DESTINATION_ID='darkness_beau'
-  );
-
